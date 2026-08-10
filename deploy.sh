22c799a2d446fdf0f5a4eaee78f60c2c2946e61c
Changjun Wu <3416637855@qq.com>
2026-08-10 23:25:39 +0800

    更新部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-cd "$(dirname "$0")"
+cd /var/www/html
 
 echo "📦 拉取最新代码..."
 sudo git pull
 
 echo "🚀 部署..."
 sudo rm -rf assets blogs articles music favicon.ico data
-sudo mv dist/* . && sudo rm -rf dist
+sudo mv youngblog/dist/* . && sudo rm -rf youngblog/dist
 sudo chown -R www-data:www-data /var/www/html/
 sudo chmod -R 755 /var/www/html/
 sudo systemctl restart nginx
